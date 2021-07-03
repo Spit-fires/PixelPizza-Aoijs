426e20397eb1587c9208a7d5137720a5e1d4c34d
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-07-03 21:16:01 +0200

    Feat: added invite command (#2)

---
+++ commands/info/invite.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "invite",
+	code: `
+		$color[BLUE]
+		$title[Invite]
+		$description[Here is the invite link for [$client[name]]($getBotInvite[createinvite;sendmessages;embedlinks])]
+	`
+}
