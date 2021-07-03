cd94c7d1a315b5fb545831a7ec198bc718960aed
Jaron <60853956+JaronZ@users.noreply.github.com>
2021-07-03 20:53:39 +0200

    Feat: added support command (#1)

---
+++ commands/info/support.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: "support",
+	code: `
+		$color[BLUE]
+		$title[Support server]
+		$description[Here is the invite for the [support server](https://discord.gg/pixelpizza)]
+	`
+}
